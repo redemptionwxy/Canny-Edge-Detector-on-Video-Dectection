462a0b2784fce5145113f5ffdc32d151788c2509
redemptionwxy <47026637+redemptionwxy@users.noreply.github.com>
2020-06-06 13:24:01 +0100

    Update FrametoVideo.py

--- FrametoVideo.py
+++ FrametoVideo.py
@@ -2,10 +2,10 @@ import cv2
   
 def images_to_video():
   fps = 30 
-  num_frames = 5105
+  num_frames = 3000
   img_array = []
 
-  for i in range(10000,num_frames+10001):
+  for i in range(1,num_frames+1):
     filename = "./Frame/frame"+str(i)+".jpg"
     img = cv2.imread(filename)
     img_width = img.shape[1]
@@ -15,7 +15,7 @@ def images_to_video():
       continue
     img_array.append(img)
   
-  out = cv2.VideoWriter('demo3.mp4', cv2.VideoWriter_fourcc(*'mp4v'), fps,(img_width,img_height))
+  out = cv2.VideoWriter('demo.mp4', cv2.VideoWriter_fourcc(*'mp4v'), fps,(img_width,img_height))
   
   for i in range(len(img_array)):
     out.write(img_array[i])
